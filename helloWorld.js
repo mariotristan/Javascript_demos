6abc3113d908adfc7b50a01b2308702a48d5e9ec
Mario Tristan <mariotristan@gmail.com>
2016-08-21 23:05:05 -0400

    Sunday

--- helloWorld.js
+++ helloWorld.js
@@ -2,8 +2,22 @@
 
 var x = 1;
 
-for (var index = 0; index < 10; index++) {
-    console.log(index);
+// for (var index = 0; index < 10; index++) {
+//     console.log(index);
     
-}
+// }
+
+
+var object = (function(params) {
+    var x =1;
+    var y= 2;
+    return{
+        X : x
+    }
+})
+
+
+var newobject = object();
+console.log(newobject.X);
+
 
